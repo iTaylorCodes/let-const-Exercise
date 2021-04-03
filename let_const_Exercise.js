ab5680301b33f1daa72dbf8f69c47d131bf2e542
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-04-02 19:12:16 -0700

    Completed Repo

---
+++ let_const_Exercise.js
@@ -0,0 +1,20 @@
+// ES5 Global Constants
+var PI = 3.14;
+
+PI = 42;
+
+// ES2015 Global Constants
+
+const PI = 3.14;
+
+PI = 42;
+
+// Quiz
+
+// 1. var has global and function scope and can also be redeclared; let has block scope
+
+// 2. const has block scope and cannot be reassigned
+
+// 3. const cannot be reassigned
+
+// 4. Hoisting is when JavaScript puts all hoisted items, like var variables and functions, and reads those before the rest of the script. var variable declarations are hoisted but not assignments
